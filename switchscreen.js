d36a5acc6fe30f5a27646124b931ef8892e2870d
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2022-08-20 18:05:44 +0900

    ネカフェで修正

---
+++ switchscreen.js
@@ -0,0 +1,6 @@
+var menunews = document.getElementById("menuNews");
+menunews.addEventListener('click', function () {
+    var item = document.getElementById("secNews");
+    document.querySelector("#secNews").style.display = "block";
+    log("hello");
+}, false);
